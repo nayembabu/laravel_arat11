e7a2f5f75b36c30ac6324e73a85f64ca8120c009
ABUNAYEM <babunayem@gmail.com>
2025-11-02 22:38:39 +0600

    partials setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,10 @@
-<h1>Hello Brother</h1>
+@include('partials.header')
+@include('partials.topmenu')
+@include('partials.sidebar')
+
+
+<h1>Hello Brother this is welcome Page.</h1>
+
+
+
+@include('partials.footer')
